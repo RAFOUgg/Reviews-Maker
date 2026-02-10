9defc99c6857bfa908776aa26450e42f5d3b60cc
RAFOUACTIF <bgmgaming09@gmail.com>
2026-02-10 16:03:55 +0100

    refactor(tests): improve code formatting and consistency in pipeline-wrapping.spec.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,24 +1,24 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './tests/e2e',
-  timeout: 60_000,
-  expect: {
-    timeout: 5000
-  },
-  fullyParallel: true,
-  use: {
-    actionTimeout: 0,
-    baseURL: 'http://localhost:5173',
-    trace: 'on-first-retry',
-    viewport: { width: 1280, height: 800 }
-  },
-  projects: [
-    {
-      name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome']
-      }
-    }
-  ]
+    testDir: './tests/e2e',
+    timeout: 60_000,
+    expect: {
+        timeout: 5000
+    },
+    fullyParallel: true,
+    use: {
+        actionTimeout: 0,
+        baseURL: 'http://localhost:5173',
+        trace: 'on-first-retry',
+        viewport: { width: 1280, height: 800 }
+    },
+    projects: [
+        {
+            name: 'chromium',
+            use: {
+                ...devices['Desktop Chrome']
+            }
+        }
+    ]
 });
