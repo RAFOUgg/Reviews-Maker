503008c27677bb0dc0c22dd836a12172b3a952e6
RAFOUACTIF <bgmgaming09@gmail.com>
2026-03-05 15:57:48 +0100

    chore: prisma migration - add texture extra fields (malleabilite, friabilite, viscosite, melting, residu)

---
+++ server-new/prisma/migrations/20260305145741_add_texture_extra_fields/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "flower_reviews" ADD COLUMN "friabiliteScore" REAL;
+ALTER TABLE "flower_reviews" ADD COLUMN "malleabiliteScore" REAL;
+ALTER TABLE "flower_reviews" ADD COLUMN "meltingScore" REAL;
+ALTER TABLE "flower_reviews" ADD COLUMN "residuScore" REAL;
+ALTER TABLE "flower_reviews" ADD COLUMN "viscositeScore" REAL;
