bacac718fb6b73b2258b159c7d372117ae6016d9
Ubuntu <ubuntu@vps-acc1787d.vps.ovh.net>
2026-03-09 08:39:42 +0000

    chore: add Prisma migration for harvest/cannabinoids fields on FlowerReview

---
+++ server-new/prisma/migrations/20260309083857_add_harvest_cannabinoids_fields_flower_review/migration.sql
@@ -0,0 +1,12 @@
+-- AlterTable
+ALTER TABLE "flower_reviews" ADD COLUMN "cbcPercent" REAL;
+ALTER TABLE "flower_reviews" ADD COLUMN "cbgPercent" REAL;
+ALTER TABLE "flower_reviews" ADD COLUMN "cbnPercent" REAL;
+ALTER TABLE "flower_reviews" ADD COLUMN "dosageUnit" TEXT;
+ALTER TABLE "flower_reviews" ADD COLUMN "modeRecolte" TEXT;
+ALTER TABLE "flower_reviews" ADD COLUMN "poidsBrut" REAL;
+ALTER TABLE "flower_reviews" ADD COLUMN "poidsNet" REAL;
+ALTER TABLE "flower_reviews" ADD COLUMN "thcvPercent" REAL;
+ALTER TABLE "flower_reviews" ADD COLUMN "trichomesAmbres" REAL;
+ALTER TABLE "flower_reviews" ADD COLUMN "trichomesLaiteux" REAL;
+ALTER TABLE "flower_reviews" ADD COLUMN "trichomesTranslucides" REAL;
